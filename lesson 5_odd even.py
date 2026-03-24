933e3a092abfe06d498d7129697ce4baa6a99e85
jharna748 <jharna.kewalramani@gmail.com>
2026-03-24 14:43:28 +0530

    commit

---
+++ lesson 5_odd even.py
@@ -0,0 +1,7 @@
+#Program to check if a number is odd or even
+num = int(input("Enter a number"))
+print("This program will check if the number is odd or even.")
+if num %2 == 0:
+    print(num,"is even.")
+else:
+    print(num, "is odd.")
